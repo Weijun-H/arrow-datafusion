3a63b60360420732fdc05e5f408a4969ae105847
Weijun-H <huangweijun1001@gmail.com>
2025-02-09 17:32:20 +0800

    feat: Added prefer_round_robin for clickbench_partitioned

--- benchmarks/src/clickbench.rs
+++ benchmarks/src/clickbench.rs
@@ -27,6 +27,9 @@ use datafusion_common::exec_datafusion_err;
 use datafusion_common::instant::Instant;
 use structopt::StructOpt;
 
+// hack to avoid `default_value is meaningless for bool` errors
+type BoolDefaultTrue = bool;
+
 /// Run the clickbench benchmark
 ///
 /// The ClickBench[1] benchmarks are widely cited in the industry and
@@ -68,6 +71,11 @@ pub struct RunOpt {
     /// If present, write results json here
     #[structopt(parse(from_os_str), short = "o", long = "output")]
     output_path: Option<PathBuf>,
+
+    /// If true then round robin repartitioning is used, if false then on demand repartitioning
+    /// True by default.
+    #[structopt(short = "r", long = "prefer_round_robin", default_value = "true")]
+    prefer_round_robin: BoolDefaultTrue,
 }
 
 struct AllQueries {
@@ -123,6 +131,11 @@ impl RunOpt {
             // as binary due to how it was written. Force it to strings
             parquet_options.binary_as_string = true;
         }
+
+        config
+            .options_mut()
+            .optimizer
+            .prefer_round_robin_repartition = self.prefer_round_robin;
 
         let ctx = SessionContext::new_with_config(config);
         self.register_hits(&ctx).await?;
